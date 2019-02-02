a2125ef3efdaa222fd1a2fa85e3c9d9f383132bd
Jerry Shen <jerry_shen0527@outlook.com>
2019-02-02 13:48:00 +0800

    2019/2/2

---
+++ 01_base/function.py
@@ -0,0 +1,49 @@
+def my_abs(x):
+    if not isinstance(x,(int,float)):
+        raise TypeError('bad operand type')
+    if x>0:
+        return x
+    else:
+        return -x
+
+def my_f(x):
+    pass
+    print('another function has been used')
+
+# def triple(name='Jerry',son):     #默认参数要后置
+#     input("name of son:")
+#     return name,son  #python允许多值返回，但事实上返回了一个元组
+
+def add_end_wrong(L=[]):
+    L.append('end')
+    return L
+
+def add_end_right(L=None):  #默认形参用不可变对象
+    if not L:
+        L=[]
+    L.append('end')
+    return L
+
+# print(add_end_right())
+# print(add_end_right())
+# print(add_end_right())
+
+def sum(*num):
+    result=0
+    for number in num:
+        result+=number
+    return result
+
+L=[1,2,3]
+
+print(sum(*L))  #类似指针的取内容
+
+def sumagain(*num,a):
+    print(a,num)
+
+sumagain(3,6,a=2)   #定义原型：func(参数，可变参数，关键字参数，命名关键字参数)
+
+def keyword(name,age,**kw):
+    print(name,age,kw)
+
+keyword('Jerry',18,city='Hefei',single=True)
---
+++ 01_base/main.py
@@ -0,0 +1,3 @@
+# from abstest import my_abs
+import math
+print('%.2f'%math.sqrt(2))
